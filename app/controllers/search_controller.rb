83ef333e174365837f02df3cbd1c31347cce9568
Max Ribbans <ribbans.max@gmail.com>
2021-02-26 11:22:28 -0500

    Add facade spec

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,5 @@
 class SearchController < ApplicationController
   def index
+    @station = StationFacade.closest_station(params[:location])
   end
 end
---
+++ spec/models/facades/station_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe StationFacade do
+  it 'can find the closest station' do
+    
+  end
+end
